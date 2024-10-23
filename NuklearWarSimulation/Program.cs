c5116d5f7585b7f96df88e86b00fcdd5fbc07908
ridvanbozoglu <ridvanbozoglu.5@gmail.com>
2024-10-23 09:59:00 +0300

    task is done.

--- NuklearWarSimulation/Program.cs
+++ NuklearWarSimulation/Program.cs
@@ -7,21 +7,32 @@ namespace NuklearWarSimulation
     {
         static void Main(string[] args)
         {
-            int distance = 60;
+            int distance = 5000;
             System.Console.WriteLine("Resources " + distance + "  meters away.");
-            Hero hero = new Kahraman(100,3);
-            Bug originalBug = new Bug();
-            originalBug.Health = 100;
-            originalBug.AttackDamage = 3;
+
+            Hero hero = new Kahraman(1000, 10);
+            
+            Bug bug = new Bug();
+            Lion lion = new Lion();
+            Zombi zombi = new Zombi();
+            bug.Health = 50;
+            bug.AttackDamage = 2;
+            lion.Health = 100;
+            lion.AttackDamage = 15;
+            zombi.Health = 300;
+            zombi.AttackDamage = 7;
             
             List<Enemy> enemies = new List<Enemy>
             {
-                EnemyFactory.CreateInstance(originalBug, 5),
-                EnemyFactory.CreateInstance(originalBug, 50),
-                EnemyFactory.CreateInstance(originalBug, 25)
+                EnemyFactory.CreateInstance(zombi, 1681),
+                EnemyFactory.CreateInstance(bug, 276),
+                EnemyFactory.CreateInstance(bug, 489),
+                EnemyFactory.CreateInstance(lion, 1527),
+                EnemyFactory.CreateInstance(lion, 2865),
+                EnemyFactory.CreateInstance(zombi, 3523),
             };
-            Simulation.Simulate(hero, enemies, 60);
-            
+
+            Simulation.Simulate(hero, enemies, distance);
         }
     }
 }
--- NuklearWarSimulation/enemyModels/Enemy.cs
+++ NuklearWarSimulation/enemyModels/Enemy.cs
@@ -3,14 +3,38 @@
 namespace NuklearWarSimulation
 {
     public abstract class Enemy : IEntity
-    {
+    {   
+        public int _health;
+        public int _attackDamage;
+
         public string Name { get; set; }
         public int Position { get; set; }
-        public int Health { get; set; }
-        public int AttackDamage { get; set; }
+        public int Health 
+        { 
+            get
+            { 
+                return _health; 
+            }
+            set
+            {
+                System.Console.WriteLine(Name +" has " + value + " health.");
+                _health = value;
+            }
+        }
+        public int AttackDamage 
+        { 
+            get
+            { 
+                return _attackDamage; 
+            } 
+            set
+            {
+                System.Console.WriteLine(Name +" has " + value + " attack damage.");
+                _attackDamage = value;
+            }
+        }
         public bool IsAlive { get; set;}
         
-
         public Enemy(){}
         public Enemy(int health, int attackDamage)
         {
